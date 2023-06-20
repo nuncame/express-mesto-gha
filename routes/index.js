547a5036cc9f26970ab0421794c3aac09d484194
Victoria Markova <vii.nuncame@gmail.com>
2023-06-20 16:41:13 +0300

    fix: tests-3

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,16 @@ const router = require('express').Router();
 const userRoutes = require('./users');
 const cardRoutes = require('./cards');
 
-router.get('*', function(req, res){
-  return res
-          .status(404)
-          .send({ message: "Указанная страница не существует." });
+// router.get('*', function(req, res){
+//   return res
+//           .status(404)
+//           .send({ message: "Указанная страница не существует." });
+// });
+router.use((req, res, next) => {
+  next({
+      status: 404,
+      message: "Указанная страница не существует.",
+  });
 });
 router.use(userRoutes);
 router.use(cardRoutes);
